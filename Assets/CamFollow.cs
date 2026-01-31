00d751d61136c948ab0cc4398f69f55cca29f136
Romano McBride <romano.mcbride@unity3d.com>
2026-01-31 14:48:06 -0500

    player and camera are now part of playerStart

--- Assets/CamFollow.cs
+++ Assets/CamFollow.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class CamFollow : MonoBehaviour
 {
-    public GameObject player;
+    private GameObject player;
 
     public float maxCameraAdjustmentSpeed = 5.0f;
     public float cameraAdjustmentSpeedAcceleration = 5.0f;
@@ -11,7 +11,9 @@ public class CamFollow : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //find player in level
+        PlayerInput p = FindFirstObjectByType<PlayerInput>();
+        player = p.gameObject;
     }
 
     // Update is called once per frame
