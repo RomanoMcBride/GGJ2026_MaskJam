4303fc9e9093ace42817dd7d61ddcf802939058a
Romano McBride <romano.mcbride@unity3d.com>
2026-02-01 16:57:55 -0500

    added agents to level 2 and fixed game over

--- Assets/LevelComponents/LevelExit/LevelExit.cs
+++ Assets/LevelComponents/LevelExit/LevelExit.cs
@@ -12,16 +12,17 @@ public class LevelExit : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-	    if (stateManager)
+	    if (other.gameObject.name == "Player")
 	    {
-		    if (other.gameObject.name == "Player")
+		    if (stateManager)
 		    {
 			    stateManager.ChangeState("levelCompleted");
 		    }
+		    else
+		    {
+			    Debug.Log("Level completed. No game state manager found. This could be because the level has been started directly, without the main menu");
+		    }
 	    }
-	    else
-	    {
-		    Debug.Log("Level completed. No game state manager found. This could be because the level has been started directly, without the main menu");
-	    }
+	    
     }
 }
