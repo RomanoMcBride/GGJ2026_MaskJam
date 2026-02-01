1c5e8bad9c013c31cd9a59fa1ad86d840fcf88ca
Romano McBride <romano.mcbride@unity3d.com>
2026-02-01 12:50:47 -0500

    added masks to level 2 and fixed skin color

--- Assets/PlayerState.cs
+++ Assets/PlayerState.cs
@@ -5,22 +5,28 @@ public class PlayerState : MonoBehaviour
 	public bool wearingMask;
 	public MaskType currentMaskType;
 	public SkinnedMeshRenderer maskMeshRenderer;
+	public GameObject nakedPlayer;
+	public GameObject maskedPlayer;
+	private Animator animator;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+	    //player starts naked, without mask
+        nakedPlayer.SetActive(true);
+        maskedPlayer.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     
 
     public void PickUpMask(MaskType maskType)
     {
+	    
 	    Debug.Log("Picking up mask: " + maskType );
 	    currentMaskType = maskType;
 	    //change color of mask
@@ -29,6 +35,10 @@ public class PlayerState : MonoBehaviour
 	    materials[1].color = maskType.maskColor;
 	    
 	    maskMeshRenderer.materials = materials;
+	    
+	    //switch to the masked player model
+	    nakedPlayer.SetActive(false);
+	    maskedPlayer.SetActive(true);
 	    
     }
 }
